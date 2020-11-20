88e36ea7774a351464de7166122a7f1d648657d1
Shichen Lu <shichenl2@gmail.com>
2020-11-20 00:49:42 -0500

    q1 and half of q2

--- assignment2_student_version/starter_code.py
+++ assignment2_student_version/starter_code.py
@@ -4,6 +4,11 @@ from matplotlib import pyplot as plt
 import csv
 import os
 
+# Shichen Lu Added
+import glob
+import matplotlib
+# matplotlib.use("TkAgg")
+
 class FrameCalib:
     """Frame Calibration
 
@@ -187,12 +192,13 @@ def get_stereo_calibration(left_cam_mat, right_cam_mat):
 
     return stereo_calib
 
-if __name__ == "__main__":
+
+def example():
     ## Input
     left_image_dir = os.path.abspath('./training/left')
     right_image_dir = os.path.abspath('./training/right')
     calib_dir = os.path.abspath('./training/calib')
-    sample_list = ['000001', '000002', '000003', '000004','000005', '000006', '000007', '000008', '000009', '000010']
+    sample_list = ['000001', '000002', '000003', '000004', '000005', '000006', '000007', '000008', '000009', '000010']
 
     ## Output
     output_file = open("P3_result.txt", "a")
@@ -200,25 +206,107 @@ if __name__ == "__main__":
 
     ## Main
     for sample_name in sample_list:
-        left_image_path = left_image_dir +'/' + sample_name + '.png'
-        right_image_path = right_image_dir +'/' + sample_name + '.png'
+        left_image_path = left_image_dir + '/' + sample_name + '.png'
+        right_image_path = right_image_dir + '/' + sample_name + '.png'
 
-        img_left = cv.imread(left_image_path, 0)
-        img_right = cv.imread(right_image_path, 0)
+        # Already grayscale
+        img_left = cv.imread(left_image_path, cv.IMREAD_GRAYSCALE)
+        img_right = cv.imread(right_image_path, cv.IMREAD_GRAYSCALE)
 
         # TODO: Initialize a feature detector
-        sift_detector = cv.SIFT_create()
-        img_left_gray = cv.cvtColor(img_left, cv.COLOR_BGR2GRAY)
-        kp, des = sift_detector.detectAndCompute(img_left_gray, None)
-        img_right_gray = cv.cvtColor(img_right, cv.COLOR_BGR2GRAY)
-        kp, des = sift_detector.sift.detectAndCompute(img_right_gray, None)
-        # TODO: Perform feature matching
+        sift_detector = cv.SIFT_create(nfeatures=1000)
+        kp_left, des_left = sift_detector.detectAndCompute(img_left, None)
+        kp_right, des_right = sift_detector.detectAndCompute(img_right, None)
 
-        # TODO: Perform outlier rejection
+        img_left_with_kp = 1
+        img_left_with_kp = cv.drawKeypoints(img_left, kp_left, img_left_with_kp,
+                                            flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv.imwrite('sift_keypoints.jpg', img_left_with_kp)
+        # # TODO: Perform feature matching
+        #
+        # # TODO: Perform outlier rejection
+        #
+        # # Read calibration
+        # frame_calib = None
+        # stereo_calib = None
+        #
+        # # Find disparity and depth
+        # pixel_u_list = [] # x pixel on left image
+        # pixel_v_list = [] # y pixel on left image
+        # disparity_list = []
+        # depth_list = []
+        # for i, match in enumerate(matches):
+        #     pass
+        #
+        # # Output
+        # for u, v, disp, depth in zip(pixel_u_list, pixel_v_list, disparity_list, depth_list):
+        #     line = "{} {:.2f} {:.2f} {:.2f} {:.2f}".format(sample_name, u, v, disp, depth)
+        #     output_file.write(line + '\n')
+        #
+        # # Draw matches
+        # img = cv.drawMatches(img_left, kp_left, img_right, kp_right, matches, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        # plt.imshow(img)
+        # plt.show()
+
+    output_file.close()
+
+
+def q1():
+    ## Input
+    img_paths = glob.glob("./test/left/*") + glob.glob("./training/left/*")
+
+    ## Detect Keypoints
+    for img_path in img_paths:
+        # Already grayscale
+        img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
+
+        # TODO: Initialize a feature detector
+        sift_detector = cv.SIFT_create(nfeatures=1000)
+        kp, des = sift_detector.detectAndCompute(img, None)
+        img_with_kp = cv.drawKeypoints(img, kp, None,
+                                            flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv.imwrite(f'{img_path[2:].replace("/", "_")}_keypoints.jpg', img_with_kp)
+
+
+def q2():
+    ## Input
+    left_image_dir = os.path.abspath('./test/left')
+    right_image_dir = os.path.abspath('./test/right')
+    calib_dir = os.path.abspath('./test/calib')
+    sample_list = ['000011', '000012', '000013', '000014', '000015']
+
+    ## Output
+    output_file = open("P2_results.txt", "a")
+    output_file.truncate(0)
+
+    ## Find keypoints + matches
+    for sample_name in sample_list:
+        left_image_path = left_image_dir + '/' + sample_name + '.png'
+        right_image_path = right_image_dir + '/' + sample_name + '.png'
+
+        # Already grayscale
+        img_left = cv.imread(left_image_path, cv.IMREAD_GRAYSCALE)
+        img_right = cv.imread(right_image_path, cv.IMREAD_GRAYSCALE)
+
+        # Get features
+        sift_detector = cv.SIFT_create(nfeatures=1000)
+        kp_left, des_left = sift_detector.detectAndCompute(img_left, None)
+        kp_right, des_right = sift_detector.detectAndCompute(img_right, None)
+        # Fix bug where sometimes the detector returns 1001 features
+        kp_left, des_left, kp_right, des_right = kp_left[0:1000], des_left[0:1000], kp_right[0:1000], des_right[0:1000]
+
+        # Matching
+        bf_matcher = cv.BFMatcher()
+        mask_mtx = np.zeros((1000, 1000), dtype=np.uint8)
+        for i in range(1000):
+            for j in range(1000):
+                if abs(kp_left[i].pt[1] - kp_right[j].pt[1]) < 5:
+                    mask_mtx[i][j] = 1
+        matches = bf_matcher.match(des_left, des_right, mask_mtx)
 
         # Read calibration
-        frame_calib = None
-        stereo_calib = None
+        frame_calib = read_frame_calib(calib_dir + '/' + sample_name + '.txt')
+        stereo_calib = get_stereo_calibration(frame_calib.p2, frame_calib.p3)
 
         # Find disparity and depth
         pixel_u_list = [] # x pixel on left image
@@ -226,7 +314,12 @@ if __name__ == "__main__":
         disparity_list = []
         depth_list = []
         for i, match in enumerate(matches):
-            pass
+            pt_left = kp_left[match.queryIdx].pt
+            pt_right = kp_right[match.trainIdx].pt
+            pixel_u_list.append(pt_left[0])
+            pixel_v_list.append(pt_left[1])
+            disparity_list.append(int(pt_left[0] - pt_right[0]))
+            depth_list.append(0)
 
         # Output
         for u, v, disp, depth in zip(pixel_u_list, pixel_v_list, disparity_list, depth_list):
@@ -238,5 +331,8 @@ if __name__ == "__main__":
         plt.imshow(img)
         plt.show()
 
-    output_file.close()
 
+
+if __name__ == "__main__":
+    # q1()
+    q2()
